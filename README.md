fc50ccf93022e10ca661b0074ab6e8c62c3b66e8
Alasdair Mercer <mercer.alasdair@gmail.com>
2017-02-25 23:12:03 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+     .d8888b.                            d8b                  888                            888
+    d88P  Y88b                           Y8P                  888                            888
+    Y88b.                                                     888                            888
+     "Y888b.    .d88b.  888d888 888  888 888  .d8888b .d88b.  888      .d88b.   8888b.   .d88888  .d88b.  888d888
+        "Y88b. d8P  Y8b 888P"   888  888 888 d88P"   d8P  Y8b 888     d88""88b     "88b d88" 888 d8P  Y8b 888P"
+          "888 88888888 888     Y88  88P 888 888     88888888 888     888  888 .d888888 888  888 88888888 888
+    Y88b  d88P Y8b.     888      Y8bd8P  888 Y88b.   Y8b.     888     Y88..88P 888  888 Y88b 888 Y8b.     888
+     "Y8888P"   "Y8888  888       Y88P   888  "Y8888P "Y8888  88888888 "Y88P"  "Y888888  "Y88888  "Y8888  888
+
+[ServiceLoader](https://github.com/Skelp/node-service-loader) is a service provider loader.
+
+[![Build](https://img.shields.io/travis/Skelp/node-service-loader/develop.svg?style=flat-square)](https://travis-ci.org/Skelp/node-service-loader)
+[![Coverage](https://img.shields.io/coveralls/Skelp/node-service-loader/develop.svg?style=flat-square)](https://coveralls.io/github/Skelp/node-service-loader)
+[![Dependencies](https://img.shields.io/david/Skelp/node-service-loader.svg?style=flat-square)](https://david-dm.org/Skelp/node-service-loader)
+[![Dev Dependencies](https://img.shields.io/david/dev/Skelp/node-service-loader.svg?style=flat-square)](https://david-dm.org/Skelp/node-service-loader#info=devDependencies)
+[![License](https://img.shields.io/npm/l/service-loader.svg?style=flat-square)](https://github.com/Skelp/node-service-loader/blob/master/LICENSE.md)
+[![Release](https://img.shields.io/npm/v/service-loader.svg?style=flat-square)](https://www.npmjs.com/package/service-loader)
+
+* [Install](#install)
+* [API](#api)
+* [Bugs](#bugs)
+* [Contributors](#contributors)
+* [License](#license)
+
+## Install
+
+``` bash
+$ npm install --save service-loader
+```
+
+You'll need to have at least [Node.js](https://nodejs.org) 4 or newer.
+
+## API
+
+### `load(service[, packageName])`
+
+TODO: Document
+TODO: Example(s)
+
+### `version`
+
+The current version of ServiceLoader.
+
+``` javascript
+const ServiceLoader = require('service-loader')
+
+ServiceLoader.version
+=> "0.1.0alpha"
+```
+
+## Bugs
+
+If you have any problems with ServiceLoader or would like to see changes currently in development you can do so
+[here](https://github.com/Skelp/node-service-loader/issues).
+
+## Contributors
+
+If you want to contribute, you're a legend! Information on how you can do so can be found in
+[CONTRIBUTING.md](https://github.com/Skelp/node-service-loader/blob/master/CONTRIBUTING.md). We want your suggestions
+and pull requests!
+
+A list of ServiceLoader contributors can be found in
+[AUTHORS.md](https://github.com/Skelp/node-service-loader/blob/master/AUTHORS.md).
+
+## License
+
+See [LICENSE.md](https://github.com/Skelp/node-service-loader/raw/master/LICENSE.md) for more information on our MIT
+license.
+
+© 2017 [Skelp](https://skelp.io)
+<img align="right" width="16" height="16" src="https://cdn.rawgit.com/Skelp/skelp-branding/master/assets/logo/base/skelp-logo-16x16.png">
---
+++ src/service-loader.js
@@ -0,0 +1,349 @@
+/*
+ * Copyright (C) 2017 Alasdair Mercer, Skelp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict'
+
+const debug = require('debug')('service-loader')
+const forOwn = require('lodash.forown')
+const pacscan = require('pacscan')
+const path = require('path')
+const whoIsThere = require('knockknock')
+
+/**
+ * The regular expression used to check whether the string representation of a function indicates that it is in fact an
+ * ECMAScript class.
+ *
+ * @private
+ * @type {RegExp}
+ */
+const rClass = /^class\s+/
+
+/**
+ * TODO: Document
+ * TODO: Mention iterable nature
+ * TODO: Mention singleton nature
+ *
+ * @public
+ */
+class ServiceLoader {
+
+  /**
+   * Creates an instance of {@link ServiceLoader} for the specified <code>service</code> using the <code>options</code>
+   * provided.
+   *
+   * If <code>service</code> is a function, then the name of that function will be used to lookup service providers. If
+   * no <code>packageName</code> option is specified, then the name of the package that called this method will be used
+   * instead.
+   *
+   * Consumers can control what modules/packages are considered during the search for the calling package via the
+   * <code>knockknock</code> option, however, the <code>limit</code> will always be overridden to <code>1</code>.
+   *
+   * @param {string|Function} service - the name of the service (or the service function, only if not anonymous) whose
+   * providers are to be loaded
+   * @param {ServiceLoader~Options} [options] - the options to be used (may be <code>null</code>)
+   * @return {ServiceLoader} The {@link ServiceLoader} to be used to load providers for <code>service</code>.
+   * @throws {Error} If <code>service</code> is not provided or the package name could not be resolved from the caller
+   * and the <code>packageName</code> option was not specified.
+   * @public
+   * @static
+   */
+  static load(service, options) {
+    return new ServiceLoader(service, options)
+  }
+
+  /**
+   * Finds the module that was responsible for calling {@link ServiceLoader} using <code>knockknock</code> and the
+   * <code>options</code> provided.
+   *
+   * @param {?knockknock~Options} options - the options to be passed to <code>knockknock</code>
+   * @return {?knockknock~Caller} The caller information for the module that was responsible calling
+   * {@link ServiceLoader} (may be <code>null</code> if no caller could be found).
+   * @private
+   * @static
+   */
+  static _findCaller(options) {
+    options = Object.assign({}, options, { limit: 1 })
+
+    const excludes = [ 'service-loader' ]
+
+    options.excludes = options.excludes ? excludes.concat(options.excludes) : excludes
+
+    return whoIsThere.sync(options)[0]
+  }
+
+  /**
+   * Returns whether the specified object is an ECMAScript class.
+   *
+   * @param {*} obj - the object to be checked (may be <code>null</code>)
+   * @return {boolean} <code>true</code> if <code>obj</code> is a class; otherwise <code>false</code>.
+   * @private
+   * @static
+   */
+  static _isClass(obj) {
+    return typeof obj === 'function' && rClass.test(obj.toString())
+  }
+
+  /**
+   * Parses the optional input <code>options</code> provided, normalizing options and applying default values, where
+   * needed.
+   *
+   * @param {?ServiceLoader~Options} options - the input options to be parsed (may be <code>null</code> if none were
+   * provided)
+   * @returns {ServiceLoader~Options} A new options object parsed from <code>options</code>.
+   * @private
+   * @static
+   */
+  static _parseOptions(options) {
+    if (!options) {
+      options = {}
+    }
+
+    let packageName = options.packageName
+    if (!packageName) {
+      const caller = ServiceLoader._findCaller(options.knockknock)
+      packageName = caller != null && caller.pkg != null ? caller.pkg.name : null
+    }
+
+    return {
+      knockknock: options.knockknock,
+      packageName
+    }
+  }
+
+  /**
+   * Creates an instance of {@link ServiceLoader} for the specified <code>service</code> using the <code>options</code>
+   * provided.
+   *
+   * If <code>service</code> is a function, then the name of that function will be used to lookup service providers. If
+   * no <code>packageName</code> option is specified, then the name of the package that called this constructor will be
+   * used instead.
+   *
+   * Consumers can control what modules/packages are considered during the search for the calling package via the
+   * <code>knockknock</code> option, however, the <code>limit</code> will always be overridden to <code>1</code>.
+   *
+   * @param {string|Function} service - the name of the service (or the service function, only if not anonymous) whose
+   * providers are to be loaded
+   * @param {ServiceLoader~Options} [options] - the options to be used (may be <code>null</code>)
+   * @throws {Error} If <code>service</code> is not provided or the package name could not be resolved from the caller
+   * and the <code>packageName</code> option was not specified.
+   * @public
+   */
+  constructor(service, options) {
+    options = ServiceLoader._parseOptions(options)
+
+    const serviceName = typeof service === 'function' ? service.name : service
+    const packageName = options.packageName
+
+    if (!serviceName) {
+      throw new Error('service must be provided')
+    }
+    if (!packageName) {
+      throw new Error('packageName option must be provided as cannot resolve calling package')
+    }
+
+    /**
+     * The parsed options for this {@link ServiceLoader}.
+     *
+     * @private
+     * @type {ServiceLoader~Options}
+     */
+    this._options = options
+
+    /**
+     * The name of the package whose named service the providers are to be loaded by this {@link ServiceLoader}.
+     *
+     * @private
+     * @type {string}
+     */
+    this._packageName = packageName
+
+    /**
+     * The file paths of loaded providers mapped to their corresponding loaded provider.
+     *
+     * This map is only assigned and populated the first time that this {@link ServiceLoader} is iterated over and is
+     * held as a cache to speed up future iterations over this {@link ServiceLoader} and to maintain the singleton
+     * nature of service providers.
+     *
+     * That said; this map can be cleared at any time by calling {@link #reload}.
+     *
+     * @private
+     * @type {?Map.<string, *>}
+     */
+    this._providers = null
+
+    /**
+     * The name of the service whose providers are to be loaded by this {@link ServiceLoader}.
+     *
+     * @private
+     * @type {string}
+     */
+    this._serviceName = serviceName
+
+    debug('Loaded ServiceLoader for "%s" service in "%s" package', serviceName, packageName)
+  }
+
+  /**
+   * @override
+   */
+  *[Symbol.iterator]() {
+    if (!this._providers) {
+      const packages = pacscan.sync({
+        includeParents: true,
+        path: __filename
+      })
+      const providers = new Map()
+
+      packages.forEach((pkg) => this._loadPackage(pkg, providers))
+
+      this._providers = providers
+    }
+
+    yield* this._providers.values()
+  }
+
+  /**
+   * Clears any service providers that have been previously loaded by this {@link ServiceLoader}.
+   *
+   * @return {void}
+   * @public
+   */
+  reload() {
+    this._providers = null
+  }
+
+  /**
+   * @override
+   */
+  toString() {
+    return `ServiceLoader[${this._serviceName}]`
+  }
+
+  /**
+   * Loads the <code>package.json</code> file for the package provided and then loads any matching service providers
+   * into <code>providers</code>.
+   *
+   * All services that are <b>not</b> associated with the service and package names of this {@link ServiceLoader} will
+   * be ignored.
+   *
+   * @param {pacscan~Package} pkg - the package whose service providers, if any, are to be loaded
+   * @param {Map.<string, *>} providers - the map of service providers to which any matching providers are to be added
+   * @return {void}
+   * @private
+   */
+  _loadPackage(pkg, providers) {
+    debug('Attempting to load package "%s"', pkg.name)
+
+    const packageJson = require(path.join(pkg.directory, 'package.json'))
+
+    forOwn(packageJson.services, (services, servicePackageName) => {
+      if (servicePackageName === this._packageName) {
+        this._loadServiceProviders(pkg, services, providers)
+      }
+    })
+  }
+
+  /**
+   * Loads the service provider at the specified <code>filePath</code>.
+   *
+   * Generally, this method simply <code>require</code>s the file, however, it does some special handling depending on
+   * the type of service provider exported. Currently, this special handling only extends to ECMAScript classes, where
+   * an instance is created and returned instead.
+   *
+   * @param {string} filePath - the path of the service provider file to be loaded
+   * @return {*} The result of loading the service provider.
+   * @private
+   */
+  _loadProvider(filePath) {
+    debug('Loading "%s" provider for "%s" service in "%s" package', filePath, this._serviceName, this._packageName)
+
+    const provider = require(filePath)
+    if (ServiceLoader._isClass(provider)) {
+      const ProviderConstructor = provider
+      return new ProviderConstructor()
+    }
+
+    return provider
+  }
+
+  /**
+   * Loads any matching service providers referenced in the specified <code>services</code> into <code>providers</code>.
+   *
+   * How the service providers are loaded depends on the type provided (e.g. ECMAScript classes are instantiated), and
+   * the result is cached within the specified <code>loader</code> to avoid subsequent provider loads for the same
+   * <code>loader</code>. This cache can be cleared by calling {@link ServiceLoader#reload} on <code>loader</code>.
+   *
+   * All services that are <b>not</b> associated with the service name for this {@link ServiceLoader} will be ignored.
+   *
+   * @param {pacscan~Package} pkg - the package to which <code>services</code> belongs
+   * @param {?ServiceLoader~Services} services - the services containing the providers to be loaded (may be
+   * <code>null</code> if none exist for the package)
+   * @param {Map.<string, *>} providers - the map of service providers to which any matching providers are to be added
+   * @return {void}
+   * @private
+   */
+  _loadServiceProviders(pkg, services, providers) {
+    forOwn(services, (service, serviceName) => {
+      service = typeof service === 'string' ? { path: service } : service
+
+      if (serviceName === this._serviceName && service && service.path) {
+        const providerPath = path.resolve(pkg.directory, service.path)
+
+        if (!providers.has(providerPath)) {
+          providers.set(providerPath, this._loadProvider(providerPath))
+        }
+      }
+    })
+  }
+
+}
+
+module.exports = ServiceLoader
+
+/**
+ * The options to be used to load service providers.
+ *
+ * @typedef {Object} ServiceLoader~Options
+ * @property {knockknock~Options} [knockknock] - The options to be passed to <code>knockknock</code> when attempting to
+ * determine the calling module (<code>limit</code> will always be overridden to <code>1</code>).
+ * @property {string} [packageName] - The name of the package for which the services are loaded.
+ */
+
+/**
+ * Contains information for an individual package extracted from its <code>package.json</code> file.
+ *
+ * @typedef {Object} ServiceLoader~Package
+ * @property {string} name - The name of the package.
+ * @property {?Object.<string, ServiceLoader~Services>} services - The services for the package.
+ */
+
+/**
+ * Contains information for an individual service provider.
+ *
+ * @typedef {Object} ServiceLoader~Service
+ * @property {string} path - The file path to the service provider relative to its package's installation directory.
+ */
+
+/**
+ * Contains information for all service providers available within an individual package.
+ *
+ * @typedef {Object.<string, ServiceLoader~Service|string>} ServiceLoader~Services
+ */
